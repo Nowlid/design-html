0452281540fd5a91d48036f5fdd399ec617d9a6c
TheRedMine TheredMaths <theredminedu51@gmail.com>
2023-05-20 14:27:55 +0200

    Panel base finished

--- v2/assets/js/panel.js
+++ v2/assets/js/panel.js
@@ -3,17 +3,33 @@ window.addEventListener('load', () => {
     const menu = document.body.querySelector('menu');
     const nav = menu.querySelector('nav');
 
+    var menuState = 0;
+
     // Expand or not menu
-    header.querySelector('button.carret').addEventListener('click', () => (
-        document.body.classList.toggle('expand-menu')
-    ));
+    header.querySelector('button.carret').addEventListener('click', () => {
+        document.body.classList[menuState ? 'add' : 'remove']('expand-menu');
+        menu.querySelectorAll('nav > li').forEach(e => e.classList.remove('expand'));
+        menuState = !menuState;
+    });
 
     // Expandable menu
     nav.querySelectorAll('li > ul').forEach(a => {
-        a.parentElement.classList.add('purple');
         a.parentElement.querySelector('a').addEventListener('click', b => {
-            b.currentTarget.classList.toggle('expand');
+            b.preventDefault();
+            menuState && a.parentElement.classList.toggle('expand');
             b.currentTarget.blur();
+        });
+    });
+
+    // Header dropdowns
+    header.querySelectorAll('.sidebar > div').forEach(a => {
+        a.querySelector('button').addEventListener('click', b => {
+            b.currentTarget.blur();
+            a.blur();
+
+            header.querySelectorAll('.dropdown').forEach(e => (
+                e.classList[e === a.querySelector('.dropdown') ? "toggle" : "remove"]('active')
+            ));
         });
     });
 });
